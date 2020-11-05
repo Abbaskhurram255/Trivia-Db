0023df995a72141618942e6e654b2618bbde998a
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-06 01:39:45 +0500

    Updates

---
+++ precache-manifest.9e0ad2388288f6416d9527a48bf5d991.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d982e8333603b11be6d25dbdd148f9",
+    "url": "/Trivia-Db/index.html"
+  },
+  {
+    "revision": "941336686f520ce1c719",
+    "url": "/Trivia-Db/static/css/main.623ddc2b.chunk.css"
+  },
+  {
+    "revision": "749f92bbc5ccd153d9bc",
+    "url": "/Trivia-Db/static/js/2.b1671510.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Trivia-Db/static/js/2.b1671510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941336686f520ce1c719",
+    "url": "/Trivia-Db/static/js/main.ec45b51e.chunk.js"
+  },
+  {
+    "revision": "5da8d7860906bf39b8a5",
+    "url": "/Trivia-Db/static/js/runtime-main.1c8bdb06.js"
+  },
+  {
+    "revision": "64562e0cacb6dbe78604dd3e46072eae",
+    "url": "/Trivia-Db/static/media/404.64562e0c.jpg"
+  },
+  {
+    "revision": "dc41d14252778b79ef3251b7cb94eb5f",
+    "url": "/Trivia-Db/static/media/coin.dc41d142.wav"
+  },
+  {
+    "revision": "e067672d83c3a50770b76d8e1427cd31",
+    "url": "/Trivia-Db/static/media/gameFinished.e067672d.wav"
+  },
+  {
+    "revision": "990c5d52e3e5055d0f74e303ebd5f626",
+    "url": "/Trivia-Db/static/media/gameStarted.990c5d52.wav"
+  },
+  {
+    "revision": "9e2f8f345a9dd40517642bc1d9407bec",
+    "url": "/Trivia-Db/static/media/joker.9e2f8f34.wav"
+  }
+]);
